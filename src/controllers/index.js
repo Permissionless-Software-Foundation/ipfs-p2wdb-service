06d881c7512be6b1bb27b5af46250bbe3848e3cf
Chris Troutner <chris.troutner@gmail.com>
2021-07-25 19:18:04 -0700

    fix(p2wdb): Fix bug that was preventing caching in MongoDB

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -8,6 +8,7 @@
 
 // Load the Clean Architecture Adapters library
 const adapters = require('../adapters')
+const validationEvent = require('../adapters/orbit/validation-event')
 
 // Load the JSON RPC Controller.
 const JSONRPC = require('./json-rpc')
@@ -19,30 +20,46 @@ const UseCases = require('../use-cases')
 // Load the REST API Controllers.
 const RESTControllers = require('./rest-api')
 
+let _this
+
 class Controllers {
   constructor (localConfig = {}) {
     this.adapters = adapters
     this.useCases = new UseCases({ adapters })
+
+    // Attach the event handler to the event.
+    validationEvent.on(
+      'ValidationSucceeded',
+      this.validationSucceededEventHandler
+    )
+
+    _this = this
   }
 
   async attachControllers (app) {
-    // Attach the REST controllers to the Koa app.
-    this.attachRESTControllers(app)
+    try {
+      // Attach the REST controllers to the Koa app.
+      this.attachRESTControllers(app)
 
-    // Start IPFS.
-    await this.adapters.ipfs.start()
+      // Start IPFS.
+      await this.adapters.ipfs.start()
 
-    // Get a handle on the IPFS node.
-    const ipfs = this.adapters.ipfs.ipfs
+      // Get a handle on the IPFS node.
+      const ipfs = this.adapters.ipfs.ipfs
 
-    // Start the P2WDB.
-    await this.adapters.p2wdb.start(ipfs)
+      // Start the P2WDB and attach the validation event handler/controller to
+      // the add-entry Use Case.
+      // console.log('About to attached validation controller...')
+      // await this.attachValidationController()
+      // console.log('...validation controller attached.')
 
-    // Start the P2WDB and attach the validation event handler/controller to
-    // the add-entry Use Case.
-    await this.attachValidationController()
+      // Start the P2WDB.
+      await this.adapters.p2wdb.start(ipfs)
 
-    this.attachRPCControllers()
+      this.attachRPCControllers()
+    } catch (err) {
+      console.error('Error in controllers/index.js/attachControllers()')
+    }
   }
 
   // Top-level function for this library.
@@ -70,36 +87,60 @@ class Controllers {
     )
   }
 
+  // Event handler that is triggered when a new entry is added to the P2WDB
+  // OrbitDB.
+  async validationSucceededEventHandler (data) {
+    try {
+      // console.log(
+      //   'ValidationSucceeded event triggering addPeerEntry() with this data: ',
+      //   data
+      // )
+
+      await _this.useCases.entry.addEntry.addPeerEntry(data)
+    } catch (err) {
+      console.error(
+        'Error trying to process peer data with addPeerEntry(): ',
+        err
+      )
+      // Do not throw an error. This is a top-level function.
+    }
+  }
+
   // Start the P2WDB and its downstream depenencies (IPFS, ipfs-coord, OrbitDB).
   // Also attach the post-validation, peer-replication event handler (controller)
   // to the Add-Entry Use Case.
-  async attachValidationController () {
-    try {
-      // Trigger the addPeerEntry() use-case after a replication-validation event.
-      adapters.p2wdb.orbit.validationEvent.on(
-        'ValidationSucceeded',
-        async function (data) {
-          try {
-            // console.log(
-            //   'ValidationSucceeded event triggering addPeerEntry() with this data: ',
-            //   data
-            // )
-
-            await useCases.entry.addEntry.addPeerEntry(data)
-          } catch (err) {
-            console.error(
-              'Error trying to process peer data with addPeerEntry(): ',
-              err
-            )
-            // Do not throw an error. This is a top-level function.
-          }
-        }
-      )
-    } catch (err) {
-      console.error('Error in controllers/index.js/startP2wdb()')
-      throw err
-    }
-  }
+  // async attachValidationController () {
+  //   try {
+  //     console.log(
+  //       'attachValidationController() this.adapters.p2wdb.orbit: ',
+  //       this.adapters.p2wdb.orbit
+  //     )
+  //
+  //     // Trigger the addPeerEntry() use-case after a replication-validation event.
+  //     this.adapters.p2wdb.orbit.validationEvent.on(
+  //       'ValidationSucceeded',
+  //       async function (data) {
+  //         try {
+  //           console.log(
+  //             'ValidationSucceeded event triggering addPeerEntry() with this data: ',
+  //             data
+  //           )
+  //
+  //           await _this.useCases.entry.addEntry.addPeerEntry(data)
+  //         } catch (err) {
+  //           console.error(
+  //             'Error trying to process peer data with addPeerEntry(): ',
+  //             err
+  //           )
+  //           // Do not throw an error. This is a top-level function.
+  //         }
+  //       }
+  //     )
+  //   } catch (err) {
+  //     console.error('Error in controllers/index.js/startP2wdb()')
+  //     throw err
+  //   }
+  // }
 }
 
 module.exports = Controllers
