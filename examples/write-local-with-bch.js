65f26781e2c5e959829cac6645b06ca4c6ed8213
Chris Troutner <chris.troutner@gmail.com>
2022-12-12 10:35:15 -0800

    fix(example): Added example for local writes

--- examples/write-local-with-bch.js
+++ examples/write-local-with-bch.js
@@ -3,3 +3,42 @@
   using BCH instead of PSF tokens to write. This exercises the local P2WDB
   code paths around native currency payments.
 */
+
+// Global npm libraries
+const BchWallet = require('minimal-slp-wallet')
+const { Write } = require('p2wdb')
+
+// Replace this private key and public address with your own. You can generate
+// new values at wallet.fullstack.cash.
+const WIF = 'L1tcvcqa5PztqqDH4ZEcUmHA9aSHhTau5E2Zwp1xEK5CrKBrjP3m'
+// BCH Address: bitcoincash:qqkg30ryje97al52htqwvveha538y7gttywut3cdqv
+// SLP Address: simpleledger:qqkg30ryje97al52htqwvveha538y7gttyz8q2dd7j
+
+// const serverURL = 'https://p2wdb.fullstack.cash'
+const serverURL = 'http://localhost:5010'
+
+// const { Write } = require('p2wdb')
+
+async function writeNode () {
+  try {
+    // Instantiate the BCH wallet.
+    const bchWallet = new BchWallet(WIF, { interface: 'consumer-api' })
+    await bchWallet.walletInfoPromise
+    await bchWallet.initialize()
+
+    const write = new Write({ bchWallet, serverURL })
+
+    // Generate the data that will be written to the P2WDB.
+    const appId = 'test'
+    const data = {
+      now: new Date(),
+      data: 'This is some test data.'
+    }
+
+    const result = await write.postEntry(data, appId)
+    console.log(`Data about P2WDB write: ${JSON.stringify(result, null, 2)}`)
+  } catch (err) {
+    console.error(err)
+  }
+}
+writeNode()
