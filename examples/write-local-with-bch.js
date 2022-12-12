3d8fc54063f2753c0f7ee8cb8648819ace2f5c84
Chris Troutner <chris.troutner@gmail.com>
2022-12-12 10:07:19 -0800

    fix(debug): Removing some debug screen spam

---
+++ examples/write-local-with-bch.js
@@ -0,0 +1,5 @@
+/*
+  This script uses the p2wdb npm library to write to the local P2WDB instance
+  using BCH instead of PSF tokens to write. This exercises the local P2WDB
+  code paths around native currency payments.
+*/
