540bea3310f16dad7b62f52c697ed0eb7fc326d6
Chris Troutner <chris.troutner@gmail.com>
2023-08-22 14:05:41 -0700

    fix(ipfs-coord-esm): Updating to v9.1.13

--- start-p2wdb.sh
+++ start-p2wdb.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
 
 # The P2WDB this app should mirror.
-export ORBITDB_NAME=/orbitdb/zdpuAwAxsMjxUTP58Tbd4vps2bTJNBoKkjs72SDLxuWjVy1Bx/psf-bch-p2wdb-keyvalue-v1.0.0-0003
+#export ORBITDB_NAME=/orbitdb/zdpuAwAxsMjxUTP58Tbd4vps2bTJNBoKkjs72SDLxuWjVy1Bx/psf-bch-p2wdb-keyvalue-v1.0.0-0003
 #export ORBITDB_NAME=test0423
 
 # The name this app uses when connecting to ipfs-coord subnet.
 export COORD_NAME=ipfs-p2wdb-dev
 
 # Customize these for your own system. Defaults are used.
-export PORT=5001 # REST API port
+export PORT=5002 # REST API port
+export IPFS_API_PORT=5001
 export IPFS_TCP_PORT=5668
 export IPFS_WS_PORT=5669
 
 # (optional) Log-in information for retrieving a JWT token from FullStack.cash.
-export GET_JWT_AT_STARTUP=0
-export FULLSTACKLOGIN=demo@demo.com
-export FULLSTACKPASS=demo
+#export GET_JWT_AT_STARTUP=0
+#export FULLSTACKLOGIN=demo@demo.com
+#export FULLSTACKPASS=demo
+
+# Run with a local instance of Cash Stack (dev environment)
+export USE_FULLSTACK=1
+export APISERVE=http://localhost:3000/v5/
+
+# Set the debug verbosity level
+export DEBUG_LEVEL=0
 
 node index.js
