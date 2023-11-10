31c02fb78f10faef9650f3944c6054aaa4dee1d0
Chris Troutner <chris.troutner@gmail.com>
2023-11-10 07:30:31 -0800

    Adding webrtc connections

--- src/adapters/ipfs/ipfs.js
+++ src/adapters/ipfs/ipfs.js
@@ -131,7 +131,8 @@ class IpfsAdapter {
           listen: [
             '/ip4/127.0.0.1/tcp/0',
             `/ip4/0.0.0.0/tcp/${this.config.ipfsTcpPort}`,
-            `/ip4/0.0.0.0/tcp/${this.config.ipfsWsPort}/ws`
+            `/ip4/0.0.0.0/tcp/${this.config.ipfsWsPort}/ws`,
+            `/webrtc`
           ]
         },
         transports: [
